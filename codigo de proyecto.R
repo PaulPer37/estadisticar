c43e6502503f32df06d5b901897a1106c75588bb
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2024-08-16 22:53:01 -0500

    a

---
+++ codigo de proyecto.R
@@ -0,0 +1,49 @@
+library(readxl)
+file.choose()
+datos <- "C:\\Users\\RUCO HOUSE\\OneDrive\\Desktop\\pipo\\espol\\s3\\esta\\Proyecto_Estadística1.xlsx"
+excel_sheets(datos)
+
+
+datos1 <- read_excel(datos)
+head(datos1)
+library(ggplot2)
+library(dplyr)
+?"$"
+
+hoja <- datos1$hojas
+riego1 <- datos1$Riego
+min(hoja)
+max(hoja)
+mean(hoja) #media de hojas = 2.48
+sd(hoja) #desviacion estandar de hojas = 1.12
+hist(hoja)
+boxplot(hoja)
+chisq.test(hoja)
+
+#Independencia del sustrato
+table(datos1$hojas, datos1$Sustrato)
+
+
+prop.table(table(datos1$hojas, datos1$Sustrato))
+
+ggplot(data= datos1, aes(x= hojas, fill= Sustrato)) + 
+  geom_bar()
+chisq.test(x= datos1$hojas, y= datos1$Sustrato)
+
+#Independecia del riego
+table(datos1$hojas, datos1$Riego)
+
+
+prop.table(table(datos1$hojas, datos1$Riego))
+
+ggplot(data= datos1, aes(x= hojas, fill= Riego)) + 
+  geom_bar()
+?chisq.test
+chisq.test(x= datos1$hojas, y= datos1$Riego)
+
+#Creacion de tablas para la comparacion de medias y varianzas
+
+
+
+
+            
