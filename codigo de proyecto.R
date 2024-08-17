8515ba431d6a733dfc09d410bccf1f985c0b445b
PaulPer37 <133935955+PaulPer37@users.noreply.github.com>
2024-08-16 23:14:41 -0500

    up

--- codigo de proyecto.R
+++ codigo de proyecto.R
@@ -1,6 +1,6 @@
 library(readxl)
 file.choose()
-datos <- "C:\\Users\\RUCO HOUSE\\OneDrive\\Desktop\\pipo\\espol\\s3\\esta\\Proyecto_Estadística1.xlsx"
+datos <- "C:\\Users\\RUCO HOUSE\\OneDrive\\Desktop\\pipo\\espol\\s3\\esta\\estadisticar\\Proyecto_Estadística1.xlsx"
 excel_sheets(datos)
 #lo que sea que sea
 
@@ -11,7 +11,7 @@ library(dplyr)
 ?"$"
 
 hoja <- datos1$hojas
-riego1 <- datos1$Riego
+riego <- datos1$Riego
 min(hoja)
 max(hoja)
 mean(hoja) #media de hojas = 2.48
@@ -43,7 +43,25 @@ chisq.test(x= datos1$hojas, y= datos1$Riego)
 
 #Creacion de tablas para la comparacion de medias y varianzas
 
+#comprobar binomial
+n_total <- 300 * 5  # Total de plantas (300 grupos, 5 plantas por grupo)
+x_exitos <- sum(datos1$hojas == 3)  # Total de plantas con 3 hojas
 
+# Probabilidad teórica de éxito (aquí debes colocar la probabilidad teórica esperada)
+p_teorica <- 0.3  # Ejemplo, reemplaza con la probabilidad teórica real
+
+# Realizar la prueba binomial
+prueba_binomial <- binom.test(x = x_exitos, n = n_total, p = p_teorica)
+
+# Resultados de la prueba
+print(prueba_binomial)
+
+# Interpretar resultados
+if (prueba_binomial$p.value < 0.05) {
+  cat("Los datos no se ajustan bien a la distribución binomial con la probabilidad teórica.\n")
+} else {
+  cat("Los datos se ajustan bien a la distribución binomial con la probabilidad teórica.\n")
+}
 
 
             
